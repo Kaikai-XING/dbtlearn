5bc1b0772a397a832c956b338b3e4b476bfe2ec3
Kaikai Xing <kaikai.xing.fl@gmail.com>
2024-03-13 11:21:50 +0100

    Update Basic Models, ephemeral, view, table & incremental table

---
+++ models/src/src_reviews.sql
@@ -0,0 +1,17 @@
+{{
+    config(
+        materialized = "ephemeral"
+    )
+}}
+
+WITH raw_reviews AS (
+    SELECT * FROM AIRBNB.RAW.RAW_REVIEWS
+)
+SELECT
+    listing_id,
+    date as review_date,
+    reviewer_name,
+    comments as review_text,
+    sentiment as review_sentiment
+FROM
+    raw_reviews
